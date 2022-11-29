2e41d3c4ddc347595b4872666a14b384f3066d15
Laur <laurentiuvieru50@gmail.com>
2022-11-29 21:15:09 +0200

    update

--- js/script.js
+++ js/script.js
@@ -1,18 +1,29 @@
 
 
-let menuTab = document.querySelectorAll('.nav-item a');
-console.log(menuTab);
-console.log(menuTab[0]);
-console.log(menuTab[0].attributes[2]);
+// let menuTab = document.querySelectorAll('.nav-item a');
+// console.log(menuTab);
+// console.log(menuTab[0]);
+// console.log(menuTab[0].attributes[2]);
 
-menuTab.forEach(tab => {
-    tab.addEventListener('click', () => {
-        console.log(`test`);
-        // cand dau click, sa verifice daca #href == id of section
-
-    })
-})
+// let sectionAtt = document.querySelectorAll('section')
+// console.log(sectionAtt);
+// console.log(sectionAtt[0].id);
 
 
+// menuTab.forEach(tab => {
+//     tab.addEventListener('click', () => {
+//         console.log(`click`);
+//         window.location.href=`#${sectionAtt[0].id}`;
+//         console.log(`after-click`);
+//         return false;
 
+
+//         // cand dau click, sa verifice daca #href == id of section
+//     })
+// })
+
+
+function handleSectionRedirect(sectionNameId) {
+    window.location.href = sectionNameId;
+}
 
